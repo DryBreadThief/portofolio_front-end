abb7fc03d36258621f8e29310e02e9771ee4c590
DryBreadThief <theodoros.niarchos.p@gmail.com>
2026-05-09 09:54:21 +0000

    added home,navbar,footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Me } from './me/me';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: 'me', pathMatch: 'full' },
+    { path: 'me', component: Me },
+    { path: '**', redirectTo: '' }
+
+];
